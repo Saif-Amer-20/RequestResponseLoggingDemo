341b9e2576467ad63dd026b40812abd85447b2a3
Saif Amer <30495588+Saif-Amer-20@users.noreply.github.com>
2021-06-12 22:18:45 +0300

    Add project files.

---
+++ RequestResponseLoggingDemo.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RequestResponseLoggingDemo.Web.Models;
+
+namespace RequestResponseLoggingDemo.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public ActionResult<Employee> GetByID(int id)
+        {
+            try
+            {
+                if (id == 0)
+                    return BadRequest("error");
+                var employee = new Employee()
+                {
+                    ID = id,
+                    FirstName = "firstName",
+                    LastName = "lastName",
+                    DateOfBirth = DateTime.Now.AddYears(-30)
+                };
+
+                return Ok(employee);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+            
+           
+        }
+    }
+}
